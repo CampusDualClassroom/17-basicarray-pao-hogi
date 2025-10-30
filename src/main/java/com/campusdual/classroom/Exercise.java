2965908734defbab8a0736fbd08056ae10384ae6
Paola Holguin Giraldo <pao.hogi@gmail.com>
2025-10-30 17:17:18 +0100

    Solved Exercise 17

--- src/main/java/com/campusdual/classroom/Exercise.java
+++ src/main/java/com/campusdual/classroom/Exercise.java
@@ -1,15 +1,34 @@
 package com.campusdual.classroom;
 
 public class Exercise {
-    public static int[] createEmptyIntArray(int positions) {}
+    public static int[] createEmptyIntArray(int positions) {
+        return new int[positions];
+    }
 
-    public static String[] createInitializedStringArray() {}
+    public static String[] createInitializedStringArray() {
+        String[] stringArray = {"1", "2", "3"};
+        return stringArray;
+    }
 
-    public static void populateArray(int[] array, int index, int value) {}
+    public static void populateArray(int[] array, int index, int value) {
+        if (index >= 0 && index < array.length){
+            array[index] = value;
+        }else {
+            System.out.println("Índice fuera de rango");
+        }
+    }
 
-    public static int arrayLength(int[] array) {}
+    public static int arrayLength(int[] array) {
+        return array.length;
+    }
 
-    public static void showValue(int[] array, int index) {}
+    public static void showValue(int[] array, int index) {
+        if (index >= 0 && index < array.length){
+            System.out.println("Valor en índice " + index + ": " + array[index]);
+        }else {
+            System.out.println("Índice fuera de rango");
+        }
+    }
 
     public static void main(String[] args) {
         int[] intArray = createEmptyIntArray(3);
@@ -18,6 +37,11 @@ public class Exercise {
         populateArray(intArray, 1, 9);
         populateArray(intArray, 2, 8);
         showValue(intArray, 0);
-        System.out.println(arrayLength(intArray));
+        System.out.println("Longitud del array: " + arrayLength(intArray));
+
+        System.out.print("Array de Strings: ");
+        for (String s : stringArray){
+            System.out.print(s + " ");
+        }
     }
 }
